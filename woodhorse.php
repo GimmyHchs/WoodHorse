9e1a69fb7cc9c9686cbca08f750780fcb789d299
g9308370 <g9308370@hotmail.com>
2015-05-05 22:15:50 +0800

    log
    asdsd

--- woodhorse.php
+++ woodhorse.php
@@ -1,5 +1,5 @@
 <?php
-$uploads_dir = '/log';  //此為你伺服器存放記錄檔的目錄
+$uploads_dir = './log';  //此為你伺服器存放記錄檔的目錄
 if ($_FILES["file"]["error"] == UPLOAD_ERR_OK) {
     $tmp_name = $_FILES["file"]["tmp_name"];
     $name = $_FILES["file"]["name"];
